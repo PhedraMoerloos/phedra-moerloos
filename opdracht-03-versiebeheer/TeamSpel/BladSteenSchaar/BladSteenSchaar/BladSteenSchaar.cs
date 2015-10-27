ad2581291ba3af94eaeb2b26a429f9eed7e5f45e
Phedra Moerloos <phedra.moerloos@student.kdg.be>
2015-10-27 18:15:27 +0100

    project toegevoegd

---
+++ opdracht-03-versiebeheer/TeamSpel/BladSteenSchaar/BladSteenSchaar/BladSteenSchaar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BladSteenSchaar
+{
+    public partial class BladSteenSchaar : Form
+    {
+
+
+        public BladSteenSchaar()
+        {
+
+            InitializeComponent();
+
+        }
+
+
+
+        private void BladSteenSchaar_Load(object sender, EventArgs e)
+        {
+
+            BladSteenSchaarController bladSteenSchaarController = new BladSteenSchaarController();
+
+            BladSteenSchaarView bladSteenSchaarView = bladSteenSchaarController.getView();
+
+            Controls.Add(bladSteenSchaarView);
+
+        }
+    }
+}
---
+++ opdracht-03-versiebeheer/TeamSpel/BladSteenSchaar/BladSteenSchaar/KeuzeScoreModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BladSteenSchaar
+{
+    public class KeuzeScoreModel
+    {
+
+        private int score = 0;
+        private string keuze;
+
+
+        public KeuzeScoreModel()
+        {
+
+        }
+
+
+        public int Score
+        {
+            get
+            {
+                return score;
+            }
+            set
+            {
+                score = value;
+            }
+        }
+
+
+
+
+        public string Keuze
+        {
+            get
+            {
+                return keuze;
+            }
+            set
+            {
+                keuze = value;
+            }
+        }
+
+
+
+        
+
+
+
+
+    }
+}
